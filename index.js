0f72d915663ee84647d4316b960f4d044e95dbea
Amr Mohammed <amrali13255@gmail.com>
2025-05-09 10:30:25 +0300

    Add welcome route to base URL

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ const Port = 5000;
 App.use(express.json());
 App.use("/api/courses", courseRouter);
 
+App.get("/", (req, res) => res.send("Course Management API is running. Go to /api/courses"));
+
 conn();
+
 App.listen(Port, () => console.log(`Server is Running At Port => ${Port}`));
